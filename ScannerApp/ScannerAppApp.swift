5f84c8ea3c007a64c209683eb1a391734a2090f4
Aziz Bibitov <azizbibitov11@gmail.com>
2024-08-29 00:09:47 +0500

    proj created

---
+++ ScannerApp/ScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerAppApp.swift
+//  ScannerApp
+//
+//  Created by Aziz Bibitov on 28.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
